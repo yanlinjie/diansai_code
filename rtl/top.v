e086277daf977c644ca3aeab1587f02bad0db3c1
yanlinjie <1486167127@qq.com>
2025-07-26 04:47:39 +0800

    仿真了正弦波相加后的分离，并加入dac接口，目前还未确定dac输出时钟频率应该为多少。仿真结果正确，但是代码在dac输出会有一点小瑕疵，主要是由于每次adc采样的信号数据，对应的相位不同。

--- rtl/top.v
+++ rtl/top.v
@@ -6,7 +6,9 @@ module top (
     output                              AD0_CLK                    ,
     input                               reset_n                    ,
     output             [   7:0]         DA0_Data                   ,
-    output                              DA0_Clk                     
+    output                              DA0_Clk                    ,
+    output             [   7:0]         DA1_Data                   ,
+    output                              DA1_Clk                     
 );
 
 
@@ -39,16 +41,14 @@ wire                   [  47:0]         m_axis_data_tdata          ;
 wire                   [  47:0]         fft_abs                    ;
 wire                                    m_axis_data_tvalid         ;
 wire                   [  15:0]         m_axis_data_tuser          ;
-reg                    [   9:0]         addr                       ;
-reg                    [  47:0]         data                       ;
-reg                                     wen                        ;
-wire                   [  47:0]         out_data                   ;
+
 wire                                    locked                     ;
 
 /*pll*/
 pll upll(
     .clk_out1                          (AD0_CLK                   ),//35M
     .clk_out2                          (DA0_Clk                   ),
+    .clk_out3                          (DA1_Clk                   ),
     .reset                             (rst                       ),
     .locked                            (locked                    ),
     .clk_in1                           (clk                       ) 
@@ -129,12 +129,14 @@ localparam                              wait_config = 1            ;
 localparam                              wait_adc_data = 2          ;
 localparam                              send_data = 3              ;
 localparam                              fft_data_to_bram = 4       ;
+localparam                              wait_cordic_down = 8       ;
+localparam  process_data = 9;
 localparam                              addr_to_zero = 5           ;
 localparam                              bram_data_to_ifft = 6      ;
 localparam                              wait_new_adc_data = 7      ;
 
 
-reg [4:0] state ;
+reg [5:0] state ;
 wire s_axis_config_tready;
 // reg  s_axis_config_tvalid;
 always @(posedge clk ) begin
@@ -143,12 +145,14 @@ always @(posedge clk ) begin
         s_axis_data_tlast <= 0;
         s_axis_data_tvalid <=0;
         wen <= 0;  
+        wen_1 <= 0;  
         // s_axis_config_tvalid <= 0;
     end else begin
         case (state)
             idle : begin
                     state <= wait_config;
                     addr <= 0;
+                    addr_1 <= 0;
                     s_axis_data_tlast <= 0;
                     ifft_s_axis_data_tlast <= 0;
                     ifft_s_axis_data_tvalid <= 0;
@@ -177,21 +181,56 @@ always @(posedge clk ) begin
                                 end 
                             end
             end
-            fft_data_to_bram   : begin
+            fft_data_to_bram   : begin //write to two bram
                             s_axis_data_tvalid <=0;
                             if (m_axis_data_tvalid) begin
                                 addr <= m_axis_data_tuser;
                                 data <= m_axis_data_tdata;
                                 wen <= 1;  
+
+                                addr_1 <= m_axis_data_tuser;
+                                data_1 <= m_axis_data_tdata;
+                                wen_1 <= 1;  
                             end
-                            if (m_axis_data_tlast) begin
-                                state <= addr_to_zero; 
+                            if (m_axis_data_tlast) begin //输出last信号后，应该还没完全计算完成吧 开根计算有一个时间差值
+                                state <= wait_cordic_down; 
                             end
             end 
-            
+            wait_cordic_down : begin
+                            addr <= 0;
+                            wen <= 0;
+                            addr_1 <= 0;
+                            wen_1 <= 0;
+                            if (cordic_down) begin //开根完成
+                                state <= process_data ;
+                            end
+            end
+            process_data : begin //目前只考虑两个正弦波相加
+                            addr <= addr + 1;
+                            data <= 0;
+                            wen <= 1;
+                            addr_1 <= addr_1 + 1;
+                            data_1 <= 0;
+                            wen_1 <= 1;
+                            if (addr == (max1_idx - 1)) begin
+                                // data <= 0;
+                                wen <= 0;
+                            end 
+                            if (addr_1 == (max2_idx - 1)) begin
+                                wen_1 <= 0;
+                                // data_1 <= 0;
+                            end
+                            if (addr == 1023) begin
+                                state <= addr_to_zero;
+                            end
+                             
+            end
+
             addr_to_zero : begin //清零addr
                             addr <= 0;
                             wen <= 0;
+                            addr_1 <= 0;
+                            wen_1 <= 0;
                             state <= bram_data_to_ifft;
             end
             bram_data_to_ifft : begin //这里用的是bram read data to ifft
@@ -199,10 +238,12 @@ always @(posedge clk ) begin
                             if (ifft_s_axis_data_tready) begin
                                 ifft_s_axis_data_tvalid <=1;
                                 addr <= addr + 1;
+                                addr_1 <= addr_1 + 1;
                                 if (addr== 1023) begin
                                     state <= wait_new_adc_data;//输入结束后 等待数据输出
                                     ifft_s_axis_data_tlast <= 0;
                                     addr <= 0;
+                                    addr_1 <= 0;
                                 end 
                             end       
             end 
@@ -267,7 +308,7 @@ cordic_0 u_cordic_0(
 
 );
 
-
+/***************可以扩展为信号检测 最后需要分辨三角波和正弦波******************/
 //分离 针对于两个sin相加 no uesd
 reg [31:0] max1_val, max2_val;
 reg [9:0] max1_idx, max2_idx;
@@ -290,31 +331,63 @@ always @(posedge clk) begin
                 max2_val <= m_axis_dout_tdata;
                 max2_idx <= num;
             end
-        end 
+        end else if ( state == addr_to_zero) begin //!记得清0
+                max1_val <= 0;
+                max2_val <= 0;
+                max1_idx <= 0;
+                max2_idx <= 0;
+        end
     end
 end
 
-
+reg cordic_down;
 always @(posedge clk) begin
     if (rst) begin
-        num<=10'h0; 
-    end  else if (m_axis_dout_tvalid) begin
-        num<=num + 1;
-    end else num <= 10'h0;
+        num <= 10'h0; 
+        cordic_down <= 0;
+    end  else begin        
+    if (m_axis_dout_tvalid) begin
+        num <= num + 1;
+        if (num == 1022) begin
+            cordic_down <= 1;
+        end
+    end else begin
+            num <= 10'h0;
+            cordic_down <= 0;
+        end 
+    end
+
 end
 
-/***************************************/
-
+/******************two bram to store fft data*********************/
+reg                    [   9:0]         addr                       ;
+reg                    [  47:0]         data                       ;
+reg                                     wen                        ;
+wire                   [  47:0]         out_data                   ;
 //存入FFT后的数据 48位 
 blk_mem_gen_1 u_dist_mem_gen_1024x24(
     .addra                             (addr                      ),//addr [9:0]
     .dina                              (data                      ),//data [47:0]
     .clka                              (clk                       ),
-    .ena                               (1'b1                      ), 
+    .ena                               (1'b1                      ),
     .wea                               (wen                       ),//1 w ; 0 r
     .douta                             (out_data                  ) //[47:0]
 );
 
+
+reg                    [   9:0]         addr_1                     ;
+reg                    [  47:0]         data_1                     ;
+reg                                     wen_1                      ;
+wire                   [  47:0]         out_data_1                 ;
+//存入FFT后的数据 48位 
+blk_mem_gen_1 u_dist_mem_gen_1024x24_1(
+    .addra                             (addr_1                    ),//addr [9:0]
+    .dina                              (data_1                    ),//data [47:0]
+    .clka                              (clk                       ),
+    .ena                               (1'b1                      ),
+    .wea                               (wen_1                     ),//1 w ; 0 r
+    .douta                             (out_data_1                ) //[47:0]
+);
 
 /****************************ifft1 start**********************************************************/
 
@@ -337,12 +410,44 @@ ifft u_ifft1(
     .ifft_s_axis_data_tlast            (ifft_s_axis_data_tlast    ),
     //dac
     .dac_clk                           (DA0_Clk                   ),
-    .dac_data                          (DA0_Data                  ) //o
+    .dac_data                          (dac_data                  ) //o
 );
 
+wire signed [7:0] dac_data;
+assign DA0_Data = dac_data + 128 ;
 
 
 /***********************ifft1 end***************************************************************/
 
+//!最后的dac信号需要有一个抬高 相当于要加上128 ，0 和 128 
+//两个ifft应该只有数据是可以不一样的，其他的信号感觉可以完全共用
+/****************************ifft2 start**********************************************************/
+
+wire                                    ifft_s_axis_config_tready  ;
+wire                   [  47:0]         ifft_s_axis_data_tdata_1     ;
+reg                                     ifft_s_axis_data_tvalid    ;
+wire                                    ifft_s_axis_data_tready    ;
+reg                                     ifft_s_axis_data_tlast     ;
+
+assign ifft_s_axis_data_tdata_1 = out_data_1;
+
+
+ifft u_ifft2(
+    .clk                               (clk                       ),//i
+    .rst                               (rst                       ),//i
+    .ifft_s_axis_config_tready         (ifft_s_axis_config_tready ),//o
+    .ifft_s_axis_data_tdata            (ifft_s_axis_data_tdata_1  ),
+    .ifft_s_axis_data_tvalid           (ifft_s_axis_data_tvalid   ),
+    .ifft_s_axis_data_tready           (ifft_s_axis_data_tready   ),//o
+    .ifft_s_axis_data_tlast            (ifft_s_axis_data_tlast    ),
+    //dac
+    .dac_clk                           (DA1_Clk                   ),
+    .dac_data                          (dac_data_1                  ) //o
+);
+
+wire signed [7:0] dac_data_1;
+assign DA1_Data = dac_data_1 + 128 ;
+
+/***********************ifft2 end***************************************************************/
 
 endmodule
