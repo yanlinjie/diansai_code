7658a74a7e4e3e7fb6ad7c967268b3dfee514586
yanlinjie <1486167127@qq.com>
2025-07-25 16:21:46 +0800

    加入了ifft，修复了之前的数据符号bug，前一版本代码没做数据符号处理。

--- rtl/top.v
+++ rtl/top.v
@@ -30,20 +30,20 @@ wire signed [7:0] spo;
 reg [9:0] a;
 
 // reg [3:0] state;
-reg s_axis_data_tvalid;
-wire signed [7:0] re_data_in;
-reg s_axis_data_tlast;
-wire signed[23:0] re_data;
-wire signed[23:0] im_data;
-wire  [47:0] m_axis_data_tdata;
-wire [47:0]fft_abs;
-wire m_axis_data_tvalid;
-wire [15:0] m_axis_data_tuser;
-reg [9:0] addr;
-reg [47:0] data;
-reg wen;
-wire  [47:0] out_data;
-wire locked;
+reg                                     s_axis_data_tvalid         ;
+wire            signed [   7:0]         re_data_in                 ;
+reg                                     s_axis_data_tlast          ;
+wire            signed [  23:0]         re_data                    ;
+wire            signed [  23:0]         im_data                    ;
+wire                   [  47:0]         m_axis_data_tdata          ;
+wire                   [  47:0]         fft_abs                    ;
+wire                                    m_axis_data_tvalid         ;
+wire                   [  15:0]         m_axis_data_tuser          ;
+reg                    [   9:0]         addr                       ;
+reg                    [  47:0]         data                       ;
+reg                                     wen                        ;
+wire                   [  47:0]         out_data                   ;
+wire                                    locked                     ;
 
 /*pll*/
 pll upll(
@@ -119,16 +119,22 @@ blk_mem_gen_0 u_blk_mem_gen_0(
     .doutb                             (bram_doutb                ) //[7:0
 );
 
-
+wire signed [7:0] adc_signed;
+assign adc_signed = $signed(bram_doutb) - 8'sd128;
 
 /*fft状态机*/
 localparam                              idle = 0                   ;
 localparam                              wait_config = 1            ;
 localparam                              wait_adc_data = 2          ;
-localparam                              wait_tready = 3            ;
+localparam                              wait_tready = 3            ;//no used
 localparam                              send_data = 4              ;
-localparam                              fft_last_data = 5          ;
-localparam                              wait_new_adc_data = 6      ;
+localparam                              fft_last_data = 5          ;//no used
+localparam                              fft_data_to_depose = 6     ;//no used
+localparam                              fft_data_to_dram = 7       ;
+localparam addr_to_zero = 11;
+localparam                              dram_data_to_ifft = 8      ;
+localparam                              wait_new_adc_data = 10     ;
+
 
 reg [4:0] state ;
 wire s_axis_config_tready;
@@ -138,12 +144,16 @@ always @(posedge clk ) begin
         state <= idle;
         s_axis_data_tlast <= 0;
         s_axis_data_tvalid <=0;
+        wen <= 0;  
         // s_axis_config_tvalid <= 0;
     end else begin
         case (state)
             idle : begin
                     state <= wait_config;
+                    addr <= 0;
                     s_axis_data_tlast <= 0;
+                    ifft_s_axis_data_tlast <= 0;
+                    ifft_s_axis_data_tvalid <= 0;
                     s_axis_data_tvalid <=0;
                     bram_addrb <= 0;
             end 
@@ -155,36 +165,53 @@ always @(posedge clk ) begin
                         end
             wait_adc_data :begin
                             if (adc_state == adc_wait_fft) begin
-                                state <= wait_tready;
+                                state <= send_data;
                             end
             end             
-            wait_tready: begin
-                            if (s_axis_data_tready ) begin
-                                state <= send_data;//to fft
-                                bram_addrb <= 0;
-                                s_axis_data_tlast <= 0;
-                            end
-                        end
-            send_data: begin
+            send_data: begin //给fft输入data
                             if (s_axis_data_tready) begin
                                 s_axis_data_tvalid <=1;
                                 bram_addrb <= bram_addrb + 1;//
                                  if (bram_addrb== 1023) begin
-                                    state <= fft_last_data;//输入结束后 等待输出
+                                    state <= fft_data_to_dram;//输入结束后 等待数据输出
                                     s_axis_data_tlast <= 0;
                                     bram_addrb <= 0;
-                                    // s_axis_data_tvalid <=0;
                                 end 
                             end
             end
-            fft_last_data: begin
-                            s_axis_data_tvalid <=0;//一帧数据传输结束
-                            if (m_axis_data_tlast) begin
-                                state <= wait_new_adc_data;
-                        
+            fft_data_to_dram   : begin
+
+                            s_axis_data_tvalid <=0;
+                            if (m_axis_data_tvalid) begin
+                                addr <= m_axis_data_tuser;
+                                data <= m_axis_data_tdata;
+                                wen <= 1;  
                             end
+                            if (m_axis_data_tlast) begin
+                                state <= addr_to_zero; 
+                            end
+            end 
+            addr_to_zero : begin //清零addr
+                            addr <= 0;
+                            wen <= 0;
+                            state = dram_data_to_ifft;
             end
+            dram_data_to_ifft : begin //这里用的是dram 
+                            // wen <= 0;
+                            if (ifft_s_axis_data_tready) begin
+                                ifft_s_axis_data_tvalid <=1;
+                                addr <= addr + 1;
+                                if (addr== 1023) begin
+                                    state <= wait_new_adc_data;//输入结束后 等待数据输出
+                                    ifft_s_axis_data_tlast <= 0;
+                                    addr <= 0;
+                                end 
+                            end
+                            
+            end 
             wait_new_adc_data : begin
+                            ifft_s_axis_data_tvalid <=0;
+                            // wen <= 0; 
                             if (adc_state == adc_wait_data) begin
                                 state <= wait_adc_data;
                             end
@@ -206,11 +233,11 @@ xfft_0 test (
 
   // FFT 配置通道（Configuration channel）
     .s_axis_config_tdata               (8'd1                      ),// 配置数据，通常用于设置FFT参数，例如变换方向、缩放因子等
-    .s_axis_config_tvalid              (1'd1      ),// 配置数据有效信号，表示当前tdata为有效配置
+    .s_axis_config_tvalid              (1'd1                      ),// 配置数据有效信号，表示当前tdata为有效配置
     .s_axis_config_tready              (s_axis_config_tready      ),// 配置就绪信号，FFT IP核准备好接收配置数据时为高
 
   // FFT 输入数据通道（Input data channel）
-    .s_axis_data_tdata                 ({8'd0, bram_doutb}        ),// 输入的复数数据，{虚部, 实部}；
+    .s_axis_data_tdata                 ({8'd0, adc_signed }        ),// 输入的复数数据，{虚部, 实部}；
     .s_axis_data_tvalid                (s_axis_data_tvalid        ),// 输入数据有效信号，为高表示当前数据有效
     .s_axis_data_tready                (s_axis_data_tready        ),// 输入数据就绪信号，FFT准备好接收数据时为高
     .s_axis_data_tlast                 (s_axis_data_tlast         ),// 输入数据的最后一个样本指示信号，一帧数据结束时为高
@@ -245,9 +272,102 @@ cordic_0 u_cordic_0(
 );
 
 
+//分离 针对于两个sin相加 no uesd
+reg [31:0] max1_val, max2_val;
+reg [9:0] max1_idx, max2_idx;
+reg [15:0] num;//用于分离
+
+always @(posedge clk) begin
+    if (rst) begin
+        max1_val <= 0;
+        max2_val <= 0;
+        max1_idx <= 0;
+        max2_idx <= 0;
+        num<=16'h0;
+    end
+    if (m_axis_dout_tvalid  && num < 511 ) begin
+        num<=num + 1;
+        if (m_axis_dout_tdata > max1_val ) begin
+            max2_val <= max1_val;
+            max2_idx <= max1_idx;
+            max1_val <= m_axis_dout_tdata;
+            max1_idx <= num;
+        end else if (m_axis_dout_tdata > max2_val ) begin
+            max2_val <= m_axis_dout_tdata;
+            max2_idx <= num;
+        end
+    end else num <= 0;
+end
+/***************************************/
+reg [9:0] num_2;//用于存储
 
 
+always @(posedge clk) begin
+    if (rst) begin
+        num<=16'h0;
+    end  else if (m_axis_dout_tvalid) begin
+        num<=num + 1;
+    end else num <= 16'h0;
+end
 
+// reg [9:0] addr;
+// reg [47:0] data;
+// reg wen;
+// reg [47:0] out_data;
+
+//存入FFT后的数据 48位 
+blk_mem_gen_1 u_dist_mem_gen_1024x24(
+    .addra                             (addr                      ),//addr [9:0]
+    .dina                              (data                      ),//data [47:0]
+    .clka                              (clk                       ),
+    .ena                               (1'b1                      ), 
+    .wea                               (wen                       ),//1 w ; 0 r
+    .douta                             (out_data                  ) //[47:0]
+);
+
+
+/***********/
+wire                   [   7:0]         ifft_s_axis_config_tdata   ;
+wire                                    ifft_s_axis_config_tvalid  ;
+wire                                    ifft_s_axis_config_tready  ;
+wire                   [  47:0]         ifft_s_axis_data_tdata     ;
+reg                                     ifft_s_axis_data_tvalid    ;
+wire                                    ifft_s_axis_data_tready    ;
+reg                                     ifft_s_axis_data_tlast     ;
+wire                   [  79:0]         ifft_m_axis_data_tdata     ;
+wire                   [  15:0]         ifft_m_axis_data_tuser     ;
+wire                                    ifft_m_axis_data_tvalid    ;
+wire                                    ifft_m_axis_data_tready    ;
+wire                                    ifft_m_axis_data_tlast     ;
+/***********/
+assign ifft_s_axis_data_tdata = out_data;
+
+
+/*fft ip used to ifft*/
+xfft_1 uxfft_1 (
+    .aclk                              (clk                       ),// FFT IP核的时钟输入信号，使用系统时钟clk
+
+  // FFT 配置通道（Configuration channel）
+    .s_axis_config_tdata               (8'd0                      ),// 配置数据，通常用于设置FFT参数，例如变换方向、缩放因子等
+    .s_axis_config_tvalid              (1'd1                      ),// 配置数据有效信号，表示当前tdata为有效配置
+    .s_axis_config_tready              (ifft_s_axis_config_tready ),// 配置就绪信号，FFT IP核准备好接收配置数据时为高
+
+  // FFT 输入数据通道（Input data channel）
+    .s_axis_data_tdata                 (ifft_s_axis_data_tdata    ),// 输入的复数数据，{虚部, 实部}；此处虚部为0，实部为bpsk_in
+    .s_axis_data_tvalid                (ifft_s_axis_data_tvalid   ),// 输入数据有效信号，为高表示当前数据有效
+    .s_axis_data_tready                (ifft_s_axis_data_tready   ),// 输入数据就绪信号，FFT准备好接收数据时为高
+    .s_axis_data_tlast                 (ifft_s_axis_data_tlast    ),// 输入数据的最后一个样本指示信号，一帧数据结束时为高
+
+  // FFT 输出数据通道（Output data channel）
+    .m_axis_data_tdata                 (ifft_m_axis_data_tdata    ),// FFT的输出复数数据（{虚部, 实部}）
+    .m_axis_data_tuser                 (ifft_m_axis_data_tuser    ),
+    .m_axis_data_tvalid                (ifft_m_axis_data_tvalid   ),// 输出数据有效信号，FFT完成计算后输出数据时为高
+    .m_axis_data_tready                (1'd1                      ),// 输出数据就绪信号，始终为1，表示下游模块始终准备好接收数据
+    .m_axis_data_tlast                 (ifft_m_axis_data_tlast    ) // 输出数据最后一个样本的指示信号
+
+);
+wire [39:0] ifft_re_data;
+assign ifft_re_data = ifft_m_axis_data_tdata[39:0];
 
 
 
--- sim/tb.v
+++ sim/tb.v
@@ -5,7 +5,7 @@ module top_tb;
   reg clk;               // 50MHz 主时钟
   reg reset_n;           // 复位
   reg [7:0] AD0;         // 模拟ADC数据
-  wire AD0_CLK;           // 模拟ADC采样时钟
+  wire AD0_CLK;          // 模拟ADC采样时钟
 
   // 实例化 DUT（Design Under Test）
   top uut (
@@ -15,32 +15,42 @@ module top_tb;
     .AD0_CLK(AD0_CLK)
   );
 
-  // 生成主时钟 clk：周期 20ns => 50MHz
+  // 主时钟：50MHz -> 20ns 周期
   always #10 clk = ~clk;
 
-  // 生成 ADC 采样时钟 AD0_CLK：周期 28.57ns => ~35MHz
-  // always #14.28 AD0_CLK = ~AD0_CLK;
-
-  // 正弦波采样参数
+  // 正弦波参数
   integer i;
-  real pi = 3.1415926;
-  real amplitude = 127.0; // 振幅范围 ±127，输出加 128 映射到 0~255
-  real offset = 128.0;
+  integer sample_count;
+  real pi;
+  real amplitude;
+  real offset;
+  real f_sin, f_sample;
 
- initial begin
-  // 初始状态
-  clk = 0;
-  reset_n = 0;
-  AD0 = 0;
-  #100;
-  reset_n = 1;
+  initial begin
+    // 初始化
+    clk = 0;
+    reset_n = 0;
+    AD0 = 0;
 
-  // 无限循环正弦波输入
-  i = 0;
-  forever begin
-    @(posedge AD0_CLK);
-    AD0 = $rtoi(offset + amplitude * $sin(2 * pi * i / 1024.0));
-    i = (i + 1) % 1024;
+    // 配置参数
+    pi = 3.1415926;
+    amplitude = 127.0;     // ±127，对应 ±5V
+    offset = 128.0;        // 0V → 128
+    f_sample = 35000000.0; // 采样频率：35MHz
+    f_sin = 50000.0;       // 正弦波频率：50kHz
+    sample_count = f_sample / f_sin; // 一个周期采样点数：700
+
+    // 释放复位
+    #100;
+    reset_n = 1;
+
+    // 正弦波模拟ADC输入
+    i = 0;
+    forever begin
+      @(posedge AD0_CLK);
+      AD0 = $rtoi(offset + amplitude * $sin(2 * pi * i / sample_count));
+      i = (i + 1) % sample_count;
+    end
   end
-end
+
 endmodule
